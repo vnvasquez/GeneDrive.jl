42a893e43ee0ad641b693ebf34c76bac1de03f6b
vnvasquez <vnvasquez@berkeley.edu>
2022-04-06 12:17:49 -0700

    Fix tempshock callbacks

--- src/exogenous_change.jl
+++ src/exogenous_change.jl
@@ -442,24 +442,24 @@ end
 """
     mutable struct TemperatureShockData <: ExogenousChange
         node::Symbol
-        times::Vector{Float64}
-        values::Vector{Float64}
+        times::Vector{Tuple{Float64, Float64}}
+        values::Union{Float64, Vector{Float64}}
         set::diffeq.CallbackSet
     end
 
-Data for time series of temperature.
+Data for temperature shocks.
 
 # Arguments
 
   - `node::Symbol`: `Node` where temperature shock occurs.
   - `times::Vector{Tuple{Float64,Float64}}`: Vector of tuples defining shock start/stop time points. Multiple discrete shock periods may be included.
-  - `values::Vector{Float64}`: Single temperature value in °C added to the baseline temperature during the specified time period. Multiple discrete temperature changes may be included; values may be positive or negative.
+  - `values::Union{Float64, Vector{Float64}}`: Single fixed temperature value in °C or vector of variable temperature values in °C added to the baseline temperature during the specified time period. Multiple discrete temperature changes may be included; values may be positive or negative.
   - `set::diffeq.CallbackSet`
 """
 mutable struct TemperatureShockData <: ExogenousChange
     node::Symbol
     times::Vector{Tuple{Float64, Float64}}
-    values::Vector{Float64}
+    values::Union{Float64, Vector{Float64}}
     set::diffeq.CallbackSet
 end
 
@@ -484,7 +484,7 @@ function TemperatureShockData(
         push!(callbacks, diffeq.DiscreteCallback(condition_stop, affect_stop))
     end
     callback_set = diffeq.CallbackSet((), tuple(callbacks...))
-    times = collect(Iterators.flatten(times))
+    #times = collect(Iterators.flatten(times))
     return TemperatureShockData(get_name(node), times, variable_shock, callback_set)
 end
 
