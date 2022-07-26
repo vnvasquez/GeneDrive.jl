fbdd6b233a176b903e84398e059bdd4450279211
vnvasquez <vnvasquez@berkeley.edu>
2022-07-26 16:15:33 -0700

    Include additional tests

--- test/tests_decision_model.jl
+++ test/tests_decision_model.jl
@@ -1,4 +1,11 @@
 @testset "Decision Model" begin
+
+    # Shared
+    i = JuMP.optimizer_with_attributes(Ipopt.Optimizer)
+    GAE = JuMP.GenericAffExpr{Float64, VariableRef}
+    GQE = JuMP.GenericQuadExpr{Float64, VariableRef}
+
+    ## Constraints: M
     test_species = AnophelesGambiae
     test_anoph = update_population_size(stages_abiodun(), 500)
     test_mendelian = make_organisms(test_species, genetics_mendelian(), test_anoph)
@@ -30,18 +37,64 @@
         test_short_tspan;
         node_strategy=test_strategy,
         species=test_species,
-        optimizer=JuMP.optimizer_with_attributes(Ipopt.Optimizer),
-        slack_small=true,
+        optimizer=i,
     )
 
-    GAE = JuMP.GenericAffExpr{Float64, VariableRef}
-    GQE = JuMP.GenericQuadExpr{Float64, VariableRef}
     @test JuMP.num_variables(test_prob) == 463
     @test JuMP.num_constraints(test_prob, GAE, MOI.EqualTo{Float64}) == 150
     @test JuMP.num_constraints(test_prob, GAE, MOI.GreaterThan{Float64}) == 30
     @test JuMP.num_constraints(test_prob, GAE, MOI.LessThan{Float64}) == 61
     @test JuMP.num_constraints(test_prob, GQE, MOI.EqualTo{Float64}) == 90
     @test JuMP.num_constraints(test_prob, VariableRef, MOI.EqualTo{Float64}) == 90
-    @test JuMP.num_constraints(test_prob, VariableRef, MOI.GreaterThan{Float64}) == 373
+    @test JuMP.num_constraints(test_prob, VariableRef, MOI.GreaterThan{Float64}) == 370
+    @test JuMP.num_constraints(test_prob, VariableRef, MOI.LessThan{Float64}) == 10
+
+    ## Constraints: F && slack_small && _create_default_solver
+    test_constraints = ReleaseStrategy(
+        release_this_gene_index=test_release_gene,
+        release_this_life_stage=Female,
+    )
+    test_strategy = Dict(1 => test_constraints)
+
+    test_prob = create_decision_model(
+        test_node,
+        test_short_tspan;
+        node_strategy=test_strategy,
+        species=test_species,
+        slack_small=true,
+    )
+
+    @test JuMP.num_variables(test_prob) == 463
+    @test JuMP.num_constraints(test_prob, GAE, MOI.EqualTo{Float64}) == 150
+    @test JuMP.num_constraints(test_prob, GAE, MOI.GreaterThan{Float64}) == 90
+    @test JuMP.num_constraints(test_prob, GAE, MOI.LessThan{Float64}) == 121
+    @test JuMP.num_constraints(test_prob, GQE, MOI.EqualTo{Float64}) == 90
+    @test JuMP.num_constraints(test_prob, VariableRef, MOI.EqualTo{Float64}) == 80
+    @test JuMP.num_constraints(test_prob, VariableRef, MOI.GreaterThan{Float64}) == 383
     @test JuMP.num_constraints(test_prob, VariableRef, MOI.LessThan{Float64}) == 13
+
+    ## Constraints: Mixed && slack_large
+    test_constraints = ReleaseStrategy(
+        release_this_gene_index=test_release_gene,
+        release_this_life_stage=(Male,Female),
+    )
+    test_strategy = Dict(1 => test_constraints)
+
+    test_prob = create_decision_model(
+        test_node,
+        test_short_tspan;
+        node_strategy=test_strategy,
+        species=test_species,
+        optimizer=i,
+        slack_large=true,
+    )
+
+    @test JuMP.num_variables(test_prob) == 463
+    @test JuMP.num_constraints(test_prob, GAE, MOI.EqualTo{Float64}) == 160
+    @test JuMP.num_constraints(test_prob, GAE, MOI.GreaterThan{Float64}) == 120
+    @test JuMP.num_constraints(test_prob, GAE, MOI.LessThan{Float64}) == 152
+    @test JuMP.num_constraints(test_prob, GQE, MOI.EqualTo{Float64}) == 90
+    @test JuMP.num_constraints(test_prob, VariableRef, MOI.EqualTo{Float64}) == 60
+    @test JuMP.num_constraints(test_prob, VariableRef, MOI.GreaterThan{Float64}) == 430
+    @test JuMP.num_constraints(test_prob, VariableRef, MOI.LessThan{Float64}) == 40
 end
